6de3ef0932095035c6ac1701dc7a8e523243c245
branimir.marcic@pseudocode.agency <branimir.marcic@pseudocode.agency>
2025-02-09 17:56:57 +0100

    added logging, validation, exception handler

---
+++ src/BuildingBlocks/BuildingBlocks/Exceptions/ApiResponseException.cs
@@ -0,0 +1,12 @@
+﻿namespace BuildingBlocks.Exceptions;
+
+public class ApiResponseException : InternalServerException
+{
+    public ApiResponseException(string message) : base(message)
+    {
+    }
+
+    public ApiResponseException(string message, string details) : base(message, details)
+    {
+    }
+}
