666277d95f6f134a6aa71ebfb3b1c975c978d0be
branimir.marcic@pseudocode.agency <branimir.marcic@pseudocode.agency>
2025-02-09 15:17:48 +0100

    added initial Rates.API project

---
+++ src/Services/Rate/Rate.API/Program.cs
@@ -0,0 +1,9 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.Run();
